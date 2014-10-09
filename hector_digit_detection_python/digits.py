ade539b24a7ce9b288e425b21a84825648b7e05d
Opstation <opstation@hector.gkmm.informatik.tu-darmstadt.de>
2014-10-09 19:10:22 +0200

    Added thresholding => OCR now improved a little bit

--- hector_digit_detection_python/digits.py
+++ hector_digit_detection_python/digits.py
@@ -10,10 +10,12 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from hector_digit_detection_msgs.srv import *
 
-image1 = cv2.imread('/opt/hector/hydro/src/hector_sick_robot_day_2014/hector_ocr_image_compose/data/hello.jpg', 1)
-image3 = cv2.imread('/opt/hector/hydro/src/hector_sick_robot_day_2014/hector_ocr_image_compose/data/world.jpg', 1)
 pub = rospy.Publisher('super_image', Image, queue_size=10)
 target_height = 32.0
+fp = ''
+
+image1 = numpy.zeros((1,1,3), numpy.uint8)
+image3 = numpy.zeros((1,1,3), numpy.uint8)
 
 def image2digit_service(data):
     print 'Executing image2digit service ...'
@@ -25,6 +27,19 @@ def image2digit_service(data):
 
     h2,w2 = image2.shape[:2]
     image2 = cv2.resize(image2, (int(w2 * (target_height / h2)), int(target_height)))
+
+    h2,w2 = image2.shape[:2]
+    image2 = image2[:h2,:w2-15]
+    h2,w2 = image2.shape[:2]
+    image2 = cv2.adaptiveThreshold(image2, 255, 1, cv2.THRESH_BINARY, 11, 2)
+
+    #image2_ = numpy.zeros((h2,w2+w2), numpy.uint8)
+    #image2_[:h2, :w2] = image2
+    #image2_[:h2, w2:w2+w2] = image2
+    #image2 = image2_
+
+    #cv2.imshow('image2', image2)
+    #cv2.waitKey(0)
 
     h1,w1 = image1.shape[:2]
     h2,w2 = image2.shape[:2]
@@ -38,11 +53,8 @@ def image2digit_service(data):
     image_full[:h2,w1:w1+w2, 2] = image2
     image_full[:h3,w1+w2:w1+w2+w3, :3] = image3
 
-    #cv2.imshow('input image', image_full)
-    #cv2.waitKey(0)
-
-    cv2.imwrite('/opt/hector/hydro/src/hector_sick_robot_day_2014/hector_ocr_image_compose/data/img.jpg', image_full)
-    args = ('tesseract', '/opt/hector/hydro/src/hector_sick_robot_day_2014/hector_ocr_image_compose/data/img.jpg', '/opt/hector/hydro/src/hector_sick_robot_day_2014/hector_ocr_image_compose/data/tessout', '-l', 'eng', '-psm', '7')
+    cv2.imwrite(fp + '/img.jpg', image_full)
+    args = ('tesseract', fp + '/img.jpg', fp + '/tessout', '-l', 'eng', '-psm', '7')
 
     print 'Launching tesseract ...'
     popen = subprocess.Popen(args, stdout=subprocess.PIPE)
@@ -50,9 +62,8 @@ def image2digit_service(data):
     output = popen.stdout.read()
 
     print 'Determing result ...'
-
     data = ''
-    with open ("/opt/hector/hydro/src/hector_sick_robot_day_2014/hector_ocr_image_compose/data/tessout.txt", "r") as myfile:
+    with open (fp + '/tessout.txt', 'r') as myfile:
         data = myfile.read()
 
     digit = -1
@@ -72,10 +83,13 @@ def image2digit_service(data):
 if __name__ == '__main__':
     try:
         rospy.init_node('digit_detection_python', anonymous=True)
+        fp = rospy.get_param('~img_path')
 
+        image1 = cv2.imread(fp + '/hello.jpg', 1)
         h,w = image1.shape[:2]
         image1 = cv2.resize(image1, (int(w * (target_height / h)), int(target_height)))
 
+        image3 = cv2.imread(fp + '/world.jpg', 1)
         h,w = image3.shape[:2]
         image3 = cv2.resize(image3, (int(w * (target_height / h)), int(target_height)))
 
